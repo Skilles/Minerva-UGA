1c7cacaaeef7db0c46288e341eb28ca65613804a
Skilles <haloeer1@gmail.com>
2023-02-04 03:10:17 -0500

    Fix dev proxy

--- Minerva/ClientApp/src/setupProxy.js
+++ Minerva/ClientApp/src/setupProxy.js
@@ -4,16 +4,13 @@ const { env } = require('process');
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5459';
 
-const context = [
-  "/weatherforecast",
-];
 
 const onError = (err, req, resp, target) => {
     console.error(`${err.message}`);
 }
 
 module.exports = function (app) {
-  const appProxy = createProxyMiddleware(context, {
+  const appProxy = createProxyMiddleware({
     target: target,
     // Handle errors to prevent the proxy middleware from crashing when
     // the ASP NET Core webserver is unavailable
@@ -23,8 +20,9 @@ module.exports = function (app) {
     //ws: true, 
     headers: {
       Connection: 'Keep-Alive'
-    }
+    },
+    changeOrigin: true,
   });
 
-  app.use(appProxy);
+  app.use('/api', appProxy);
 };
